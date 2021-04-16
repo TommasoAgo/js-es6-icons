5a85897de2c6bb26032b78c3614b40168d9f7068
Tommaso Agostini <tommasoagostini90@gmail.com>
2021-04-16 18:03:11 +0200

    Fcendo funzioni

--- js/script.js
+++ js/script.js
@@ -107,6 +107,11 @@ const colors = [
 // Partendo dalla seguente struttura dati , 
 // mostriamo in pagina tutte le icone disponibili come da layout.
 
+// Creo un container che sia oggetto jQuery
+let iconsContainer = $('#icons-container');
+// Lo stampo
+printIcons(icons, iconsContainer);
+
 // --------- ELEMENT TEMPLATE ---------  
 {/* <div class="icon">
 	<i class="fas fa-cat"></i>
@@ -117,6 +122,55 @@ const colors = [
 
 // Milestone 2
 // Coloriamo le icone per tipo
+const colorArray = colorIcons(icons, colors);
 
 // Milestone 3
 // Creiamo una select con i tipi di icone e usiamola per filtrare le icone
+
+
+// --------- FUNCTIONS ---------
+// Scrivo una funzione che mi fa stampare tutte le icone di un Array. Container deve essere un oggetto jQuery
+function printIcons (iconsArray, container) {
+	
+	iconsArray.forEach(element => {
+
+		// Destrutturo element per prenderne i valori
+		const {name, prefix, family} = element;
+		// Uppercase
+		const nameUppercase = name.toUpperCase();
+		
+		// IconElement sarà uguale al template che mi ero preparato su html coi valori reimpostati
+		// dalla destrutturazione dell'elemento.
+		let iconElement = `
+		<div class="icon">
+				<i class="${family} ${prefix}${name}"></i>
+				<div>
+					${nameUppercase}
+				</div>
+			</div>
+		`
+		container.append(iconElement); 
+	});
+}
+
+// Scrivo una funzione che mi restituisca un Array con i colori
+function colorIcons(originalIconsArray, colorsArray) {
+	const iconTypes = getIconsType(originalIconsArray);
+}
+
+// Creo una funzione che genera un Array con dentro i tipi degli oggetti 'Icona' senza doppioni
+function getIconsType(iconsArray) {
+	// Creo un Array vuoto da popolare con i tipi
+	const typesArray = [];
+
+	// Faccio un loop forEach sull'array di icone
+	iconsArray.forEach((element) => {
+		// Metto in una variabile tutti i tipi degli oggetti
+		const elementType = element.type;
+		
+		// Faccio un if per evitare di aggiungere doppioni nell'Array
+		if (!typesArray.includes(elementType)) {
+			typesArray.push(elementType);
+		}
+	})
+}
